e82fb689b93a0c4648efdc02f26c9858ccaef994
Bruno Almeida <bruno_jaccoud@hotmail.com>
2019-08-15 10:58:59 -0300

    service de autenticacao

---
+++ dia04/client/projetotoken/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { UsuarioService } from './service/usuario.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [UsuarioService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ dia04/client/projetotoken/src/app/service/usuario.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Usuario } from 'src/app/model/usuario';
+import { map } from "rxjs/operators";
+
+const URL1: string = 'http://localhost:3000/auth'; //VIA POST
+const URL2: string = 'http://localhost:3000/adm/list'; //VIA GET
+
+@Injectable()
+export class UsuarioService{
+
+    constructor( private http: HttpClient, private router: Router ){
+
+    }
+
+    //MÉTODO PARA LOGAR E GERAR O TOKEN
+    public getLoginAndToken( usuario: Usuario ){
+
+        return this.http.post<Usuario>(`${URL1}`, usuario).pipe(map(res => { //map separa em campos e joga o valor em res
+            localStorage.setItem('tokenizer', res.token);
+            //mas pega o campo token e joga em localStorage(SET)
+            console.log(res);
+        }));
+
+    }
+
+    //MÉTODO PARA AUTENTICAR, ENTRAR NO DIRETÓRIO E LISTAR
+    public lisAuthentication(){
+        let header = new HttpHeaders(); //habilita o uso de cabeçalho
+        header = header.append('authorization', `bearer ${localStorage.getItem('tokenizer')}`); //verifica a autorização do server e através do bearer pega (GET) o token
+        return this.http.get<Usuario[]>(`${URL2}`, {headers: header});
+    }
+
+}
